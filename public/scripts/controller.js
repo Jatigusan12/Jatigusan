c10c891706aef33ce8705d8fd2f820c591c759e2
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-10-09 08:14:48 +0800

    updated

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -6,7 +6,7 @@ document.getElementById("Off").disabled = true;
 function TurnedOn(){
   var d = new Date();
 
-  client.publish("adrian/device/status", "Turned On :"+ d);
+  client.publish("adrian/device/status", "Turned Off :"+ d);
   var status = "The Device Is Currently Turned On"
   document.getElementById('Message').innerHTML = status.toString();
   document.getElementById("Off").disabled = false;
@@ -14,7 +14,7 @@ function TurnedOn(){
 
 function TurnedOff(){
   var d = new Date();
-  client.publish("adrian/device/status", "Turned Off :"+ d);
+  client.publish("adrian/device/status", "Turned On:"+ d);
   var status = "The Device Is Currently Turned Off"
   document.getElementById('Message').innerHTML = status.toString();
   document.getElementById("Off").disabled = true;
